2cf70cda0d1a149f2b6d67b23707b0b7008c0ad7
sasikumarr1 <30343745+sasikumarr1@users.noreply.github.com>
2017-08-22 07:21:59 -0400

    Create positive.c

---
+++ positive.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main
+{
+printf("Enter a value");
+scanf("%d",&a);
+ if(a>0)
+ printf("positive value");
+ else if (a<0)
+ printf("Negative value");
+ else
+ printf("Zero");
+ }
+
